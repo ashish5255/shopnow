52be7bdf3a4216d0acf6ad6b75916a8b42625bd2
Ashish <ashishadhikari5255@gmail.com>
2024-09-18 19:36:14 +0545

    Add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Routes>
+        {/* Route for different pages now */}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="place-order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/Collection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Collection = () => {
+  return <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"></div>;
+};
+
+export default Collection;
