3e24ff941f70642c05bbc5f63d5d4bf02b39029f
Ashish <ashishadhikari5255@gmail.com>
2024-09-21 21:02:54 +0545

    Add newsletter section

--- src/components/OurPolicy.jsx
+++ src/components/OurPolicy.jsx
@@ -5,7 +5,31 @@ const OurPolicy = () => {
   return (
     <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base textgray600">
       <div>
-        <img src={assets.exchange_icon} alt="" />
+        <img
+          src={assets.exchange_icon}
+          className="w-12 m-auto mb-5"
+          alt="icon"
+        />
+        <p className="font-semibold ">Easy Exchange Policy</p>
+        <p className="text-gray-400 ">We offer hassle free exchange. </p>
+      </div>
+
+      <div>
+        <img
+          src={assets.quality_icon}
+          className="w-12 m-auto mb-5"
+          alt="icon"
+        />
+        <p className="font-semibold ">7 DAYS RETURN</p>
+        <p className="text-gray-400 ">We offer free return within 7 days. </p>
+      </div>
+
+      <div>
+        <img src={assets.support_img} className="w-12 m-auto mb-5" alt="icon" />
+        <p className="font-semibold ">Best Customer Support</p>
+        <p className="text-gray-400 ">
+          We provide 24/7 support to our customer.{" "}
+        </p>
       </div>
     </div>
   );
