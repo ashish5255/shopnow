99cf53b126aa1a7b83c6b1cdb027c444ffbd2424
Ashish <ashishadhikari5255@gmail.com>
2024-09-26 18:18:12 +0545

    Complete Cart Setup

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -44,6 +44,28 @@ const ShopContextProvider = (props) => {
     return totalCount;
   };
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+
+    setCartItems(cartData);
+  };
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product.id == items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+    return totalAmount;
+  };
+
   const value = {
     products,
     currency,
@@ -52,10 +74,11 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
-    cartItems,
     addToCart,
     cartItems,
     getCartCount,
+    updateQuantity,
+    getCartAmount,
   };
 
   return (
